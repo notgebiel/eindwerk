01b32509955130757b2d442a604eb247b2d8d672
notgebiel <cloostermansemile@gmail.com>
2024-03-14 10:06:36 +0100

    started development on front-end page

--- front-end/src/routes/front-end.jsx
+++ front-end/src/routes/front-end.jsx
@@ -1,11 +1,21 @@
 import NavBar from "../components/navbar"
-import style from '../stylesheets/front-end.module.css';
+import styles from '../stylesheets/front-end.module.css';
 
 export default function Front_End() {
     return (
         <>
             <NavBar active='Front-End' />
-            <h1 className={style.header}>front-end</h1>
+            <h1 className={styles.header}>front-end</h1>
+            <div className={styles.textdiv}>
+                <h4 className={styles.text}>
+                    In dit deel gaan we het hebben over de front-end.<br/>
+                    De front-end is alles wat je kan zien en kan gebruiken, bv. deze tekst, de knoppen bovenaan, de achtergrond...<br/>
+                    <br/>
+                    <br/>
+                    
+
+                </h4>
+            </div>
         </>
     )
 }
